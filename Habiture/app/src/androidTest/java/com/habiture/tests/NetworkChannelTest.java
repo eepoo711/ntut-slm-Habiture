555cd755a6d2e0800e8248a3aff415e52ca31615
Brian <tp26610@gmail.com>
2015-05-23 15:21:09 +0800

    add Log in Profile class

--- Habiture/app/src/androidTest/java/com/habiture/tests/NetworkChannelTest.java
+++ Habiture/app/src/androidTest/java/com/habiture/tests/NetworkChannelTest.java
@@ -4,31 +4,53 @@ package com.habiture.tests;
 import android.test.AndroidTestCase;
 
 import com.habiture.NetworkChannel;
+import com.habiture.PhotoInputStream;
+import com.habiture.Profile;
+import com.habiture.exceptions.HabitureException;
 
 import java.io.InputStream;
 
 public class NetworkChannelTest extends AndroidTestCase {
-//
-//    private NetworkChannel networkChannel;
-//
-//    @Override
-//    protected void setUp() throws Exception {
-//        super.setUp();
-//
-//        networkChannel = new NetworkChannel();
-//    }
-//
-//    public void testLogin() {
-//        Profile profile = networkChannel.httpGetLoginResult("guest", "guest", "123");
-//        int uid = profile.getId();
-//        assertEquals(1, uid);
-//    }
-//
-//    public void testGetProfilePhoto() {
-//        Profile profile = networkChannel.httpGetLoginResult("guest", "guest", "123");
-//        byte[] image = networkChannel.httpGetPhoto(profile);
-//        assertNotNull(image);
-//    }
+
+    private NetworkChannel networkChannel;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        networkChannel = new NetworkChannel();
+
+        Thread.sleep(200);
+    }
+
+    public void testGetProfile() throws Exception {
+        Profile profile = getProfile();
+        assertEquals(1, profile.getId());
+        assertEquals("http://140.124.144.121/Habiture/profile/10176068_726992954019352_539454252837054186_n.jpg", profile.getPhotoUrl());
+    }
+
+    private Profile getProfile() throws HabitureException {
+        try {
+            InputStream in = networkChannel.openGetProfileConnection("guest", "guest", "123");
+            return new Profile(in);
+
+        } finally {
+            networkChannel.closeConnection();
+        }
+    }
+
+    public void testGetProfilePhoto() throws Exception{
+        try {
+            Profile profile = getProfile();
+            PhotoInputStream imageStream = networkChannel.openGetPhotoConnection(profile.getPhotoUrl());
+            boolean hasImage = imageStream.getImageBytes() > 0;
+
+            assertNotNull(imageStream);
+            assertTrue(hasImage);
+        } finally {
+            networkChannel.closeConnection();
+        }
+    }
 //
 //    public void testQueryFriends() {
 //        Profile profile = networkChannel.httpGetLoginResult("guest", "guest", "123");
@@ -60,17 +82,26 @@ public class NetworkChannelTest extends AndroidTestCase {
 //        assertNotNull(groups);
 //    }
 
-    public void testPass() {
+    public void testPass() throws Exception {
+        assertTrue(pass());
+    }
 
-        // login
-        NetworkChannel networkChannel = new NetworkChannel();
-        InputStream in = networkChannel.openGetProfileConnection("guest", "guest", "123");
-        assertNotNull(in);
-        networkChannel.closeConnection();
+    public void testPassFailed() throws  Exception {
+        assertFalse(pass());
+    }
+
+
+    private boolean login() throws HabitureException {
+        Profile profile = getProfile();
+        return true;
+    }
+
+    private boolean pass() throws HabitureException {
+
+        assertTrue(login());
 
         // pass
-        boolean success = networkChannel.postPass("{\"uid\":1,\"pid\":172 }");
-        assertTrue(success);
+        return networkChannel.postPass("{\"uid\":1,\"pid\":172 }");
     }
 
 }
