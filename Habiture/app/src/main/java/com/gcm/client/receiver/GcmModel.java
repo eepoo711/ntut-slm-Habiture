0619efd87f18573a5dbcd5a7adc253fe16fc7cec
CodusMocia <assa4415@yahoo.com.tw>
2015-05-27 10:59:49 +0800

    fix-soundpool

--- Habiture/app/src/main/java/com/gcm/client/receiver/GcmModel.java
+++ Habiture/app/src/main/java/com/gcm/client/receiver/GcmModel.java
@@ -10,7 +10,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
